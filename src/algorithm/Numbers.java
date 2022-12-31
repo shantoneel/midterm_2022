571edd503dec33052e8b16f5d28f5f5dcb804bed
shantoneel <santoneeldhar@hotmail.com>
2022-12-31 15:14:24 -0800

    new updates

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -18,8 +18,8 @@ public class Numbers {
 	 */
 
 	public static void main(String[] args) throws Exception {
-		
-		int [] num = new int[1000000];
+
+		int [] num = new int[100000];
 		storeRandomNumbers(num);
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		//Selection Sort
@@ -27,17 +27,66 @@ public class Numbers {
 		algo.selectionSort(num);
 		long selectionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
-        List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
-        printValue(numbers);
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
+		List<String> numbers = connectToSqlDB.readDataBase("selection_sort", "SortingNumbers");
+		//printValue(numbers);
+
+		//By following above, Continue for rest of the Sorting Algorithm....
+
 		int n = num.length;
 		randomize (num, n);
 		//Insertion Sort
 		algo.insertionSort(num);
 		long insertionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "insertion_sort", "SortingNumbers");
+		List<String> numbers2 = connectToSqlDB.readDataBase("insertion_sort", "SortingNumbers");
+		//printValue(numbers2);
 
-		//By following above, Continue for rest of the Sorting Algorithm....
+		//MergeSort
+		randomize (num, n);
+		algo.mergeSort(num,0,num.length-1,num.length);
+		long mergeSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Merge Sort take: " + mergeSortExecutionTime + " milli sec");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "merge_sort", "SortingNumbers");
+		List<String> numbers3 = connectToSqlDB.readDataBase("merge_sort", "SortingNumbers");
+		//printValue(numbers3);
+
+		//ShellSort
+		randomize (num, n);
+		algo.shellSort(num);
+		long shellSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Shell Sort take: " + shellSortExecutionTime + " milli sec");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "shell_sort", "SortingNumbers");
+		List<String> numbers4 = connectToSqlDB.readDataBase("shell_sort", "SortingNumbers");
+		//printValue(numbers4);
+
+		//BubbleSort
+		randomize (num, n);
+		algo.bubbleSort(num);
+		long bubbleSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Bubble Sort take: " + bubbleSortExecutionTime + " milli sec");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "bubble_sort", "SortingNumbers");
+		List<String> numbers5 = connectToSqlDB.readDataBase("bubble_sort", "SortingNumbers");
+		//printValue(numbers5);
+
+		//HeapSort
+		randomize (num, n);
+		algo.heapSort(num);
+		long heapSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Heap Sort take: " + heapSortExecutionTime + " milli sec");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "heap_sort", "SortingNumbers");
+		List<String> numbers6 = connectToSqlDB.readDataBase("heap_sort", "SortingNumbers");
+		//printValue(numbers6);
+
+		/*
+		randomize (num, n);
+		algo.bucketSort(num);
+		long bucketSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Bucket Sort take: " + bucketSortExecutionTime + " milli sec");
+		//connectToSqlDB.insertDataFromArrayToSqlTable(num, "bucket_sort", "SortingNumbers");
+		//List<String> numbers = connectToSqlDB.readDataBase("bucket_sort", "SortingNumbers");
+		 */
 
 
 
@@ -46,8 +95,10 @@ public class Numbers {
 
 
 
-
-		//Come to conclusion about which Sorting Algo is better in given data set.
+		/*Come to conclusion about which Sorting Algo is better in given data set.
+		 Answer: given this data set the worst performing algorithm was Bubble Sort.
+			The best performing algorithm was heap sort.
+		 */
 
 	}
 
--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -8,18 +8,60 @@ public class UnitTestSorting {
       This class is about Unit testing for Sorting Algorithm.
      */
     public static void main(String[] args) {
-        int [] unSortedArray = {6,9,2,5,1,0,4};
         int [] sortedArray =   {0,1,2,4,5,6,9};
+        int [] unSortedArray = {6,9,2,5,1,0,4};
+        int [] unSortedArray2 = {6,9,2,5,1,0,4};
+        int [] unSortedArray3 = {6,9,2,5,1,0,4};
+        int [] unSortedArray4 = {6,9,2,5,1,0,4};
+        int [] unSortedArray5 = {6,9,2,5,1,0,4};
+        int [] unSortedArray6 = {6,9,2,5,1,0,4};
+
         //Create Sort object
         Sort sort = new Sort();
-        //apply unsorted array to selectionSort.
-        sort.selectionSort(unSortedArray);
+
         //verify if the unsorted array is sorted by the selection sort algorithm.
         try {
-            Assert.assertEquals(sortedArray, unSortedArray);
+            Assert.assertArrayEquals(sortedArray,sort.selectionSort(unSortedArray));
         }catch(Exception ex){
             ex.getMessage();
         }
+
+        //test insertion sort
+        try {
+            Assert.assertArrayEquals(sortedArray,sort.insertionSort(unSortedArray2));
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+
+        //test quick sort
+        try {
+            Assert.assertArrayEquals(sortedArray,sort.quickSort(unSortedArray3,0,unSortedArray3.length-1));
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+        //test shell sort
+        try {
+            Assert.assertArrayEquals(sortedArray,sort.shellSort(unSortedArray4));
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+        //test merge sort
+        try {
+            Assert.assertArrayEquals(sortedArray,sort.mergeSort(unSortedArray5,0,unSortedArray5.length-1,unSortedArray5.length));
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+        //test heap sort
+        try {
+            Assert.assertArrayEquals(sortedArray,sort.heapSort(unSortedArray6));
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
 
         //Now implement Unit test for rest of the soring algorithm...................below
 
